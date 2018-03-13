f762d0bce420a6342faa72e47ca2da9d20451283
Elyud Fabián Granda Gutiérrez <lightum7@outlook.com>
2018-03-13 12:56:28 -0600

    PDF para Revision
    PDF para Revision

--- resources/views/Mod00_Administrador/pdfInventario.blade.php
+++ resources/views/Mod00_Administrador/pdfInventario.blade.php
@@ -40,23 +40,96 @@
 <div class="container" >
 
      <div class="row">
+        <div class="col-6">
+             <table class="table table-striped">
+                    <thead class="thead-dark">
+                        <tr>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($data as $inv)
+                        <tr>
+                            <td scope="row">
+                                <img src="{{ asset('/images/zarkin.png') }}" width="120px" height="52px">       
+                            </td>
+                            <td scope="row">
+                                    
+                            </td>
+                            <td scope="row">
+                            INVENTARIO DE EQUIPO DE CÓMPUTO      
+                            </td>
+                        </tr>    
+                        <tr>
+                            <td scope="row"colspan="2">
+                                Número de Equipo
+                            </td>
+                            <td scope="row">
+                                {{ $inv->numero_equipo }}      
+                            </td>
+                        </tr> 
+                        <tr>
+                            <td scope="row"colspan="2">
+                                Equipo
+                            </td>
+                            <td scope="row">
+                                {{ $inv->nombre_equipo }}  
+                            </td>
+                        </tr>   
+                        <tr>
+                            <td scope="row"colspan="2">
+                                Monitor
+                            </td>
+                            <td scope="row">
+                                {{ $inv->nombre_monitor }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td scope="row"colspan="2">
+                                Equipo Asignado a
+                            </td>
+                            <td scope="row">
+                                {{ $inv->correo}}
+                            </td>
+                        </tr>    
+                        <tr>
+                            <td scope="row"colspan="2">
+                                Tipo de Equipo
+                            </td>
+                            <td scope="row">
+                                {{ $inv->tipo_equipo }}
+                            </td>
+                        </tr>       
+                    @endforeach 
+                    </tbody>
+                </table>
+        </div>
      <div class="col-6">
-     
-            @foreach ($data as $inv)
-                Número de Equipo: {{ $inv->numero_equipo }}
-                <br>
-                Equipo:  {{ $inv->nombre_equipo }}
-                <br>
-                Monitor: {{ $inv->nombre_monitor }}
-                <br>
-                Equipo Asignado a {{ $inv->correo}}
-                <br>
-                Tipo de Equipo: {{ $inv->tipo_equipo }}
-                <br>
-            @endforeach 
-     </div>
-     <div class="col-6">
-        OK
+     <h4>Confidencialidad/Responsabilidades</h4>
+        <h6>1. ES RESPONSABILIDAD DEL USUARIO ACATAR LA PRESENTE NORMATIVIDAD EN EL USO DE LOS BIENES INFORMÁTICOS</h6>
+        <h6>2. SERÁ RESPONSABILIDAD DEL USUARIO EL USO Y CUIDADO DEL EQUIPO DE CÓMPUTO</h6>
+        <h6>3. QUEDA ESTRICTAMENTE PROHIBIDO LA DESCARGA, INSTALACION O DISTRIBUCION DE SOFTWARE QUE NO SEA PROPORCIONADO POR EL AREA DE SISTEMAS</h6>
+        <h6>4. EL ADMINISTRADOR DE SISTEMAS CUENTA CON LA FACULTAD DE AUDITAR LOS EQUIPOS EN EL MOMENTO QUE LO CONSIDERE OPORTUNO SIN QUE EXISTA LA OBLIGACIÓN DE INFORMAR AL USUARIO DE LA ACTIVIDAD A REALIZAR</h6>
+        <h6>5. LA CONSERVACION Y USO OPTIMO DEL BIEN INFORMÁTICO, ES RESPONSABILIDAD DIRECTA DEL ENCARGADO, EL CUAL DEBERÁ DAR AVISO A SISTEMAS EN CASO DE EXTRAVIO O DE DAÑO AL EQUIPO</h6>
+        <h6>6. AL TERMINO DE LAS LABORES EL RESPONSABLE VERIFICARÁ QUE EL EQUIPO SE ENCUENTREN DEBIDAMENTE APAGADO</h6>
+        <h6>7. LOS USUARIOS DE LAS COMPUTADORAS NO PODRAN CAMBIAR LA CONFIGURACION DE LOS EQUIPOS</h6>
+        <h6>8. LOS DAÑOS O SINIESTROS QUE OCURRAN AL EQUIPO DEBERAN NOTIFICARSE AL ÁREA DE SISTEMAS POR MEDIO DE UN ESCRITO PARA EVALUAR EL DAÑO</h6>
+        <h6>9. TODOS LOS USUARIOS QUE REQUIERAN DE UN ADITAMENTO PARA SU EQUIPO, DEBERÁN SOLICITARLO A EL DEPARTAMENTO DE SISTEMAS A TRAVÉS DE UN CORREO ELECTRÓNICO</h6>
+        <h6>10. EL USUARIO DE UN BIEN INFORMÁTICO, CON FACULTADES DE ACUERDO A SUS FUNCIONES , MANIFIESTA QUE CONOCE, ACEPTA Y CUMPLIRÁ LAS OBLIGACIONES Y RESPONSABILIDADES QUE IMPLICA, RESUMIDAS EN LO YA DESCRITO, DE CONFORMIDAD PARA LA ADMINISTRACIÓN.</h6> 
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <center>Nombre y Firma</center>
      </div>
      </div>
      @yield('subcontent-01')
